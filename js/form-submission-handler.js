df649cdfc9cc868e04985811d926283709c586fa
Andrew Keymolen <andrewkeymolen@gmail.com>
2024-03-25 17:29:02 +0100

    design overhaul

--- js/form-submission-handler.js
+++ js/form-submission-handler.js
@@ -1,28 +1,28 @@
-(function() {
+(function () {
   // get all data in form and return object
   function getFormData(form) {
     var elements = form.elements;
     var honeypot;
 
-    var fields = Object.keys(elements).filter(function(k) {
+    var fields = Object.keys(elements).filter(function (k) {
       if (elements[k].name === "honeypot") {
         honeypot = elements[k].value;
         return false;
       }
       return true;
-    }).map(function(k) {
-      if(elements[k].name !== undefined) {
+    }).map(function (k) {
+      if (elements[k].name !== undefined) {
         return elements[k].name;
-      // special case for Edge's html collection
-      }else if(elements[k].length > 0){
+        // special case for Edge's html collection
+      } else if (elements[k].length > 0) {
         return elements[k].item(0).name;
       }
-    }).filter(function(item, pos, self) {
+    }).filter(function (item, pos, self) {
       return self.indexOf(item) == pos && item;
     });
 
     var formData = {};
-    fields.forEach(function(name){
+    fields.forEach(function (name) {
       var element = elements[name];
 
       // singular form elements just have one value
@@ -47,7 +47,7 @@
     formData.formGoogleSendEmail
       = form.dataset.email || ""; // no email by default
 
-    return {data: formData, honeypot: honeypot};
+    return { data: formData, honeypot: honeypot };
   }
 
   function handleFormSubmit(event) {  // handles form submit without any jquery
@@ -67,27 +67,27 @@
     xhr.open('POST', url);
     // xhr.withCredentials = true;
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-          form.reset();
-          var formElements = form.querySelector(".form-elements")
-          if (formElements) {
-            formElements.style.display = "none"; // hide form
-          }
-          var thankYouMessage = form.querySelector(".thankyou_message");
-          if (thankYouMessage) {
-            thankYouMessage.style.display = "block";
-          }
-          var buttons = form.querySelectorAll("button");
-          for (var i = 0; i < buttons.length; i++) {
-            buttons[i].disabled = false;
-            buttons[i].style.backgroundColor  = "rgb(246, 163, 159)";
-          }
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        form.reset();
+        var formElements = form.querySelector(".form-elements")
+        if (formElements) {
+          formElements.style.display = "none"; // hide form
         }
+        var thankYouMessage = form.querySelector(".thankyou_message");
+        if (thankYouMessage) {
+          thankYouMessage.style.display = "block";
+        }
+        var buttons = form.querySelectorAll("button");
+        for (var i = 0; i < buttons.length; i++) {
+          buttons[i].disabled = false;
+          buttons[i].style.backgroundColor = "rgb(41, 42, 75)";
+        }
+      }
     };
     // url encode form data for sending as post data
-    var encoded = Object.keys(data).map(function(k) {
-        return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+    var encoded = Object.keys(data).map(function (k) {
+      return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
     }).join('&');
     xhr.send(encoded);
   }
@@ -109,7 +109,7 @@
     var buttons = form.querySelectorAll("button");
     for (var i = 0; i < buttons.length; i++) {
       buttons[i].disabled = true;
-      buttons[i].style.backgroundColor  = 'LightGray';
+      buttons[i].style.backgroundColor = "rgb(41, 42, 75, 0.5)";
     }
   }
 })();
